2505ba55ee503554ce51ce15b1f4112a0f818695
oscar <oscar.gonzalez@ynov.com>
2024-12-09 10:30:21 +0100

    mise en place de l'arborescence bug fixé

--- main.go
+++ main.go
@@ -9,10 +9,10 @@ import (
 
 func main() {
     mux := http.NewServeMux()
-    mux.HandleFunc("/api/artists", api.GetArtistsHandler)
-    mux.HandleFunc("/api/locations", api.GetLocationsHandler)
-    mux.HandleFunc("/api/dates", api.GetDatesHandler)
-    mux.HandleFunc("/api/relations", api.GetRelationsHandler)
+    mux.HandleFunc("/api/artist", api.GetArtistsHandler)
+    mux.HandleFunc("/api/location", api.GetLocationsHandler)
+    mux.HandleFunc("/api/date", api.GetDatesHandler)
+    mux.HandleFunc("/api/relation", api.GetRelationsHandler)
 
     log.Println("Server is running on port 8080...")
     log.Fatal(http.ListenAndServe(":8080", mux))
